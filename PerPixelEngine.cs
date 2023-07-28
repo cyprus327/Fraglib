603e3a1d9fa3f73ff1b8b99fc9201b42b722af9f
cyprus <jrcjr327@gmail.com>
2023-07-28 18:21:16 -0400

    Added DeltaTime uniform

--- PerPixelEngine.cs
+++ PerPixelEngine.cs
@@ -6,17 +6,18 @@ internal sealed class PerPixelEngine : Engine {
     public PerPixelEngine(int w, int h, string t, Func<int, int, Uniforms, uint> perPixel, Action perFrame) : base(w, h, t) {
         _perPixel = perPixel;
         _perFrame = perFrame;
-        uniforms.Time = 0f;
+        uniforms.Time = uniforms.DeltaTime = 0f;
         uniforms.Width = w;
         uniforms.Height = h;
     }
 
     private readonly Func<int, int, Uniforms, uint> _perPixel;
     private readonly Action _perFrame;
-    private Uniforms uniforms = new Uniforms();
+    private Uniforms uniforms = new();
 
     public override void Update(FrameEventArgs args) {
         uniforms.Time += (float)args.Time;
+        uniforms.DeltaTime = (float)args.Time;
         
         _perFrame();
         
@@ -36,9 +37,7 @@ internal sealed class PerPixelEngine : Engine {
                 int x = sx * PixelSize; // original x
                 for (int py = y; py < y + PixelSize; py++) {
                     for (int px = x; px < x + PixelSize; px++) {
-                        if (px >= WindowWidth || py >= WindowHeight) {
-                            continue;
-                        }
+                        if (px >= WindowWidth || py >= WindowHeight) continue;
                         Screen[py * WindowWidth + px] = fragColor;
                     }
                 }
@@ -48,6 +47,7 @@ internal sealed class PerPixelEngine : Engine {
 }
 
 public struct Uniforms {
+    public float DeltaTime;
     public float Time;
     public int Height;
     public int Width;
