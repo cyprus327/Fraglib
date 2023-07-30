d4353b9e338c6d92d1fd1d8f3814688746e976e1
cyprus <jrcjr327@gmail.com>
2023-07-29 21:01:00 -0400

    Move logic to FL

--- DrawClearEngine.cs
+++ DrawClearEngine.cs
@@ -9,47 +9,6 @@ internal sealed class DrawClearEngine : Engine {
 
     private readonly Action _program;
 
-    public void SetPixel(int x, int y, uint col) {
-        if (PixelSize == 1) {
-            Screen[y * WindowWidth + x] = col;
-            return;
-        }
-
-        int xBounds = Math.Min(x + PixelSize, WindowWidth);
-        int yBounds = Math.Min(y + PixelSize, WindowHeight);
-        unsafe {
-            fixed (uint* ptr = Screen) {
-                for (int py = y; py < yBounds; py++) {
-                    int yo = py * WindowWidth;
-                    for (int px = x; px < xBounds; px++) {
-                        ptr[yo + px] = col;
-                    }
-                }
-            }
-        }
-    }
-
-    public void FillRect(int x, int y, int w, int h, uint col) {
-        unsafe {
-            fixed (uint* ptr = Screen) {
-                for (int r = y; r < y + h; r++) {
-                    int ro = r * WindowWidth;
-                    for (int c = x; c < x + w; c++) {
-                        ptr[ro + c] = col;
-                    }
-                }
-            }
-        }
-    }
-
-    public uint GetPixel(int x, int y) {
-        return Screen[y * WindowWidth + x];
-    }
-
-    public void Clear(uint col) {
-        Array.Fill(Screen, col);
-    }
-
     public override void Update(FrameEventArgs args) {
         _program();
     }
